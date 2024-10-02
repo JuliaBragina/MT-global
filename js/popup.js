366ac9a13464a31843cdb984911940b2f45416b1
Юлия Брагина <juliabragina@MBP-Ulia.home>
2024-10-02 15:42:07 +0400

    добавлены все popup формы

--- js/popup.js
+++ js/popup.js
@@ -1,20 +1,55 @@
 document.addEventListener('DOMContentLoaded', function() {
     const popupOverlay = document.querySelector('.popup-overlay');
-    const openPopupButton = document.querySelector('.header__openPopup');
-    const closePopupButton = document.getElementById('closePopup');
+    const popupOverlayCatalog = document.querySelector('.popup-overlay-getCatalog');
+    const popupOverlayNeedProject = document.querySelector('.popup-overlay-needProject');
+
+    const openCallBackPopup = document.querySelectorAll('.openCallBackPopup');
+    const openGetSolutionsPopup = document.querySelectorAll('.openGetSolutionsPopup');
+    const openOrderSolutionPopup = document.querySelectorAll('.openOrderSolutionPopup');
+
+    const closePopupButton = document.querySelectorAll('.popup__close-button');
     
-    console.log(openPopupButton)
-    openPopupButton.addEventListener('click', function() {
-        popupOverlay.style.display = 'flex';
+    openCallBackPopup.forEach(item => {
+        item.addEventListener('click', function() {
+            popupOverlay.style.display = 'flex';
+        });
+    })
+
+    openOrderSolutionPopup.forEach(item => {
+        item.addEventListener('click', function() {
+            popupOverlayNeedProject.style.display = 'flex';
+        })
     });
 
-    closePopupButton.addEventListener('click', function() {
-        popupOverlay.style.display = 'none';
+    openGetSolutionsPopup.forEach(item => {
+        item.addEventListener('click', function() {
+            popupOverlayCatalog.style.display = 'flex';
+        })
+    });
+
+    closePopupButton.forEach(item => {
+        item.addEventListener('click', function() {
+            popupOverlay.style.display = 'none';
+            popupOverlayCatalog.style.display = 'none';
+            popupOverlayNeedProject.style.display = 'none';
+        });
     });
 
     popupOverlay.addEventListener('click', function(e) {
         if (e.target === popupOverlay) {
             popupOverlay.style.display = 'none';
+        }
+    });
+
+    popupOverlayCatalog.addEventListener('click', function(e) {
+        if (e.target === popupOverlayCatalog) {
+            popupOverlayCatalog.style.display = 'none';
+        }
+    });
+
+    popupOverlayNeedProject.addEventListener('click', function(e) {
+        if (e.target === popupOverlayNeedProject) {
+            popupOverlayNeedProject.style.display = 'none';
         }
     });
 });
