38634b7f3e5c35c6884c04325ee3c9361a461797
Юлия Брагина <juliabragina@MBP-Ulia.home>
2024-10-08 14:27:55 +0400

    поправлены стили для слайдеров

--- js/swiper.js
+++ js/swiper.js
@@ -1,15 +1,18 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
 
 document.addEventListener("DOMContentLoaded", () => {
+    const slidesPerView = {
+        reviews: 1,
+        about: 5
+    };
+
     document.querySelectorAll('.swiper').forEach(slider => {
         const uniqueClass = slider.classList[0];
 
-        const swiperContainer = slider.querySelector(`.swiper-container_${uniqueClass}`);
-        const nextButton = slider.querySelector(`.swiper-button-next_${uniqueClass}`);
-        const prevButton = slider.querySelector(`.swiper-button-prev_${uniqueClass}`);
+        const swiperContainer = slider.querySelector('.swiper-container');
+        const nextButton = slider.querySelector('.swiper-button-next');
+        const prevButton = slider.querySelector('.swiper-button-prev');
 
-        console.log(uniqueClass, swiperContainer, nextButton, prevButton);
-        
         if (swiperContainer && nextButton && prevButton) {
             new Swiper(swiperContainer, {
                 loop: true,
@@ -17,8 +20,29 @@ document.addEventListener("DOMContentLoaded", () => {
                     nextEl: nextButton,
                     prevEl: prevButton,
                 },
-                slidesPerView: uniqueClass === 'reviews' ? 1 : 4,
-                spaceBetween: 10,
+                slidesPerView: slidesPerView[uniqueClass] || 4,
+                spaceBetween: 20,
+                spaceBetween: 0,
+                breakpoints: {
+                    1300: {
+                        slidesPerView: uniqueClass === 'reviews' ? 1 : (uniqueClass === 'about' && 5) || 4,
+                    },
+                    1024: {
+                        slidesPerView: uniqueClass === 'reviews' ? 1 : 4,
+                    },
+                    768: {
+                        slidesPerView: uniqueClass === 'reviews' ? 1 : 4,
+                    },
+                    425: {
+                        slidesPerView: uniqueClass === 'reviews' ? 1 : 2,
+                    },
+                    360: {
+                        slidesPerView: 1,
+                    },
+                    320: {
+                        slidesPerView: 1,
+                    },
+                },
             });
         } else {
             console.error('Не удалось найти элементы для инициализации Swiper', { swiperContainer, nextButton, prevButton });
