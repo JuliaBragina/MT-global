9a9afee4c848f78d66e98257aefee9e109a34498
rzarekt <rzarekt@yandex.ru>
2024-10-09 19:06:59 +0700

    Перенес меню на все страницы. Кнопки теперь с классами-утилитами Добавлена анимация появлений секций

---
+++ js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let blocks = document.querySelectorAll('section:not(.no-effect)');
+
+    function checkBlocksVisibility() {
+        console.log('123')
+        let windowHeight = window.innerHeight;
+
+        blocks.forEach(block => {
+            let blockPosition = block.getBoundingClientRect().top;
+
+            if (blockPosition < windowHeight - 100) {
+                block.style.opacity = "1";
+                block.style.top = "0";
+            }
+        });
+    }
+
+    checkBlocksVisibility();
+
+    window.addEventListener('scroll', function () {
+        checkBlocksVisibility();
+    });
+});
